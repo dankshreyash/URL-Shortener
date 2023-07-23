789a2b29e7560f75d3243f3e7531e752e74efb74
random27x <da1003312@gmail.com>
2023-07-23 22:11:06 +0530

    first commit

---
+++ routes/urlroute.js
@@ -0,0 +1,71 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+const shortURL = require('../models/urlschema')
+
+
+router.get('/', async (req, res) => {
+    // res.render('index')
+
+    const shorturls = await shortURL.find()
+    res.render('index', { shorturls: shorturls })
+})
+
+router.post('/shortUrls', async (req, res) => {
+
+
+    const url = req.body.full
+    const newShortURL = new shortURL({
+        full: url
+    })
+    await newShortURL.save()
+    console.log('created', newShortURL)
+
+    res.redirect('/')
+
+})
+
+//post end
+
+
+//view data
+
+
+router.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await shortURL.findOne({ short: req.params.shortUrl })
+
+    if (shortUrl == null) {
+        return res.sendStatus(404)
+    }
+
+
+    await shortUrl.clicks++;
+    shortUrl.save()
+    res.redirect(shortUrl.full);
+
+})
+
+router.get('/delete/:id', async (req, res) => {
+    const id = req.params.id
+    /// res.send(id)
+
+    try {
+        await shortURL.deleteOne({ _id: id })
+        console.log('delete');
+        res.redirect('/')
+
+    } catch (err) {
+        console.log(err);
+    }
+
+
+
+
+})
+
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require("mongoose")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+//26.27
+
+//const writeConcern = { w: 'majority', wtimeout: 0, provenance: 'clientSupplied' };
+
+mongoose.connect("mongodb+srv://urlshrink:urlshrink@cluster0.svsyg2g.mongodb.net/urlshrinker?retryWrites=true", {
+    //     useNewUrlParser: true,
+    //    // useCreateIndex: true,
+    //     useUnifiedTopology: true,
+    //     //useFindAndModify: false
+})
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+    console.log('error');
+})
+
+db.once('open', () => {
+    console.log('establish done');
+})
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//link route
+const urlRouter = require('./routes/urlroute')
+
+
+app.use('/', urlRouter)
+
+
+
+
+app.listen(PORT, () => {
+    console.log('listening ')
+})
