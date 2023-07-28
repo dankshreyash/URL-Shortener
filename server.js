4a7db35889abaeba23ebd43e0ceb72e798299ce1
random27x <da1003312@gmail.com>
2023-07-28 18:48:47 +0530

    un-commented

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const PORT = process.env.PORT || 3000
 
 
 mongoose.connect(process.env.MONGO_URI, {
-    //     useNewUrlParser: true,
-    //    // useCreateIndex: true,
-    //     useUnifiedTopology: true,
-    //     //useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
 })
 
 const db = mongoose.connection;
